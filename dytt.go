7f9216aeb4282e666bf0a3ae71d0e529fabf658b
MT <maotong@xiaoyezi.com>
2020-08-27 19:50:49 +0800

    服务器访问

--- dytt.go
+++ dytt.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"container/list"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/axgle/mahonia"
 )
 
 // HTTPGet 得到 url 对应的内容。
@@ -26,36 +31,83 @@ func HTTPGet(url string) (result string, err error) {
 		// 累加读取的内容
 		result += string(buf[:n])
 	}
+
+	result = ConvertToString(result, "gbk", "utf-8")
 	return
 }
 
+// 编码
+func ConvertToString(src string, srcCode string, tagCode string) string {
+	srcCoder := mahonia.NewDecoder(srcCode)
+	srcResult := srcCoder.ConvertString(src)
+	tagCoder := mahonia.NewDecoder(tagCode)
+	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
+	result := string(cdata)
+	return result
+}
+
+var movies = list.New()
+var urls = list.New()
+
 func main() {
-	var serverKey = ""
+	http.HandleFunc("/getMovie", movieFunc)
+	http.HandleFunc("/start", start)
+	http.ListenAndServe("0.0.0.0:8080", nil)
+}
+
+func start(writer http.ResponseWriter, request *http.Request) {
+	movies.PushFront("釜山行")
+	urls.PushFront("https://www.dy2018.com")
+
+	var serverKey = "server 酱"
 	eventsTick := time.NewTicker(time.Duration(1) * time.Second)
 	defer eventsTick.Stop()
 	for {
 		select {
 		case <-eventsTick.C:
-			var movies = [...]string{"釜山行"}
-			var urls = [...]string{"https://www.dy2018.com"}
-
-			for _, movie := range movies {
-				for _, url := range urls {
-					result, err := HTTPGet(url)
+			fmt.Println(time.Now())
+			for movie := movies.Front(); movie != nil; movie = movie.Next() {
+				for url := urls.Front(); url != nil; url = url.Next() {
+					var movieStr = movie.Value.(string)
+					result, err := HTTPGet(url.Value.(string))
 
 					if err != nil {
 						return
 					}
-
-					var inde = strings.Index(result, movie)
-					if inde != -1 {
-						fmt.Println("没有" + movie)
+					var inde = strings.Index(result, movieStr)
+					if inde == -1 {
+						fmt.Println("没有" + movieStr)
 					} else {
-						fmt.Println("有" + movie)
-						http.Get("https://sc.ftqq.com/" + serverKey + ".send?text=" + movie + url)
+						fmt.Println("有" + movieStr)
+						movies.Remove(movie)
+						http.Get("https://sc.ftqq.com/" + serverKey + ".send?text=" + movieStr + url.Value.(string))
 					}
 				}
 			}
+		}
+	}
+}
+
+type Resp struct {
+	Code string `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+func movieFunc(writer http.ResponseWriter, request *http.Request) {
+
+	request.ParseForm()
+	movie, uError := request.Form["movie"]
+
+	var result Resp
+
+	fmt.Println(uError)
+	if movie != nil && uError {
+		movies.PushFront(movie[0])
+		result.Msg = "添加成功"
+		result.Code = "200"
+
+		if err := json.NewEncoder(writer).Encode(result); err != nil {
+			log.Fatal(err)
 		}
 	}
 }
